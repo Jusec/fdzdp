2c04914774b87295c342c576f5c8e928ab37a351
Eureka <851649659@qq.com>
2023-04-02 16:06:40 +0800

    Add Gateway

---
+++ GateWay/src/main/java/com/jusec/gateway/GatewatApplication.java
@@ -0,0 +1,19 @@
+package com.jusec.gateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName GatewatApplication
+ * @description:
+ * @Author: Mo
+ * @Date: 2023/03/31 20:12
+ * @Version 17.0.1
+ **/
+@SpringBootApplication
+public class GatewatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewatApplication.class, args);
+    }
+}
