cf5763c7073326f20578803147ec656f68c32c8e
Eureka <851649659@qq.com>
2023-04-02 16:25:10 +0800

    Add Voucher-service

---
+++ Voucher-service/src/main/java/com/jusec/voucher/config/RabbitMQConfig.java
@@ -0,0 +1,30 @@
+package com.jusec.voucher.config;
+
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName RabbitMQConfig
+ * @description:
+ * @Author: Mo
+ * @Date: 2023/03/28 15:07
+ * @Version 17.0.1
+ **/
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory){
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
+    }
+
+}
+
